32faf8360e0d31e652b2b006bfa4d1c8b182829e
IvaTsu <ivan.tsud@tretton37.com>
2023-04-06 11:26:41 +0200

    feat: add '@module-federation/nextjs-mf' and configure host next.js app

--- app-host/next.config.js
+++ app-host/next.config.js
@@ -1,8 +1,29 @@
+const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = {
+  ...nextConfig,
+  webpack(config, options) {
+    if (!options.isServer) {
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: "app-host",
+          filename: `static/chunks/remoteEntry.js`,
+          remotes: {
+            "app-host":
+              "app-host@http://localhost:3000/_next/static/chunks/remoteEntry.js",
+          },
+          exposes: {},
+          shared: {},
+        })
+      );
+    }
+    return config;
+  },
+};
