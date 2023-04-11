a76829c79d0c410de9d63cc032688f0fa02b3232
IvaTsu <ivan.tsud@tretton37.com>
2023-04-11 15:21:13 +0200

    refactor: use 'public' folder for assets

--- remote-nextjs-module/components/Pikachu.js
+++ remote-nextjs-module/components/Pikachu.js
@@ -4,7 +4,12 @@ const Pikachu = () => {
   return (
     <div>
       <p>This is the React Component hosted at Remote Module</p>
-      <Image src="/../static/pikachu.png" width={200} height={200} />
+      <Image
+        src="/images/pikachu.png"
+        width={200}
+        height={200}
+        alt="pikachu pokemon"
+      />
     </div>
   );
 };
